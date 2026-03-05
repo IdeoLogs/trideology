e5410a22cc0185a089444ab9dccb682e14564355
IdeoLogs <ideologsmail@gmail.com>
2026-03-04 22:51:05 -0500

    Add files via upload

---
+++ Trideology/Trideology/questions.js
@@ -0,0 +1,8 @@
+questions =  [
+    //individualist questions
+    {"id": 0, "question": "I am a liberal", "effects": {"lib": 1}},
+    {"id": 1, "question": "I am a communalist", "effects": {"com": 1}},
+    {"id": 2, "question": "I am a paternalist", "effects": {"pat": 1}}
+,
+
+];
